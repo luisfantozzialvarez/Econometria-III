c0b2c305d3c292dc2448214732010580ec4c3a33
Luis Antonio Fantozzi Alvarez <luisfantozzialvarez@gmail.com>
2024-05-07 19:56:11 -0300

    Procedimento de Johansen

--- codigos/cointegracao/simulacoes.R
+++ codigos/cointegracao/simulacoes.R
@@ -11,8 +11,8 @@ a = lapply(1:10000, function(x)
 }
   )
 
-slope_reg = sapply(a, function(d) coefficients(lm(y~x, data = d))[2])
+t_stat = sapply(a, function(d) {mod = summary(lm(y~x, data = d)); mod$coefficients[2,3] - gamma/mod$coefficients[2,2]})
 
 plot(a[[3]],plot.type='single',col = c('red', 'blue'), ylab = '')
 legend('topright',legend = c('y', 'x'), col = c('red','blue'), lty = c(1,1))
-plot(density(slope_reg),main = expression('Distribuição de '~hat(gamma)~' em amostras repetidas'), xlab='')
+plot(density(slope_reg),main = expression('Distribuição de '~over(hat(gamma)-gamma,se(hat(gamma)))~' em amostras repetidas'), xlab='', cex.main=0.9)
